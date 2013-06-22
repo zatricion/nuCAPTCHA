168abedc88d0e04f206dee352cbcc1672dd208b5
Michael Lauria <mlauria@caltech.edu>
2013-06-21 22:03:22 -0400

    first database connection attempt

--- models.py
+++ models.py
@@ -1,23 +1,38 @@
 """
-@package models
-module that sets up all of the MySQL tables used
+models imports app, but app does not import models so we haven't created
+any loops.
 """
+import datetime
 from peewee import *
-import nucaptcha
+from flask_peewee.auth import BaseUser # provides password helpers..
 
-class Image(nucaptcha.db.Model):
+from app import db
+
+class User(db.Model, BaseUser):
+    username = CharField()
+    password = CharField()
+    email = CharField()
+    join_date = DateTimeField(default=datetime.datetime.now)
+    active = BooleanField(default=True)
+    admin = BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.username
+
+class Image(db.Model):
     filename = CharField()
     answer   = CharField()
     
-class Secondary(nucaptcha.db.Model):
-    filename   = CharField()
+class Secondary(db.Model):
+    filename = CharField()
 
-class SecCounts(nucaptcha.db.Model):
+class SecCounts(db.Model):
     sec = ForeignKeyField(Secondary, related_name = 'counts')
     pos_count  = IntegerField()
     neg_count  = IntegerField()
     neut_count = IntegerField()
+    known      = BooleanField(default=False)
     
 
-class KnownSecondaries(nucaptcha.db.Model):
-    pass
+# class KnownSecondaries(db.Model):
+#     pass
--- nucaptcha.py
+++ nucaptcha.py
@@ -1,73 +1,20 @@
-from flask import Flask
-from flask import json, jsonify, send_file
-import glob, random
+"""
+this is the "secret sauce" -- a single entry-point that resolves the
+import dependencies.  If you're using blueprints, you can import your
+blueprints here too.
 
-from peewee import *
-from flask_peewee.db import Database
+then when you want to run your app, you point to main.py or `main.app`
+"""
+from app import app, db
 
-DATABASE = {
-    'name': 'nucaptcha',
-    'engine': 'peewee.PostgresqlDatabase',
-    'user': 'mdlauria',
-    'host': 'localhost',
-    'port': 5432
-}
-
-application = Flask(__name__) # had to call it application for elastic beanstalk
-app = application 
-app.config.from_object(__name__)
-
-
-# instantiate the db wrapper
-db = Database(app)
-
-# must wait for database instantiation
+from auth import *
+from admin import admin
+# from api import api
 from models import *
+from views import *
 
-DOMAIN = 'localhost:5000' #'nucaptcha.com'
-IM_DOMAIN = 'http://' + DOMAIN + '/images/'
-IM_COUNT = 2 # TODO: change to database query
-SEC_DOMAIN = 'http://' + DOMAIN + '/secondary/'
-SEC_COUNT = 10
-
-### DATABASE:
-#  Ground Truth
-#  image_id | text word
-#
-#  Secondary
-#  text sentence | pos_count | neut_count | neg_count | known (this is a bool)
-
-### GET request for actual captcha
-
-@app.route('/')
-def send_captcha():
-  image_id = random.randrange(0, IM_COUNT)
-  image_url = IM_DOMAIN + str(image_id)
-  
-  sec_id = random.randrange(0, SEC_COUNT)
-  sec_url = SEC_DOMAIN + str(sec_id)
-
-  # Wrap both image and sentence URLs using jsonify.
-  # Then serve both URLs in JSON format. 
-  return jsonify(image_id=image_id, image_url=image_url,
-                 sec_id=sec_id, sec_url = sec_url)
-
-
-# ADD SECURITY (can just grab images at all domains)
-@app.route('/images/<id>')
-def serve_image(id):
-  filename = 'images/' + id + '.jpeg'
-  return send_file(filename, mimetype='image/jpeg')
-  
-@app.route('/secondary/<id>')
-def serve_secondary(id):
-  filename = 'secondary/' + id + '.jpeg'
-  return send_file(filename, mimetype='image/jpeg')
-    
-### POST request with info on the answer they gave
-
-# TODO: return a Bool <correct> (sometimes require both correct)
-# TODO: if <correct> : update database
+admin.setup()
+# api.setup()
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run()
