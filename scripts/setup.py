122df718bc131d786e22b9d0405c35e3851995d0
Michael Lauria <mlauria@caltech.edu>
2013-06-24 16:01:41 -0400

    adding scripts

---
+++ scripts/setup.py
@@ -0,0 +1,15 @@
+from models import *
+import glob, re
+
+def initialize(image_dir, sec_dir):
+    User.create_table(True)
+    Image.create_table(True)
+    Secondary.create_table(True)
+    
+    # Add file names to tables
+    for file in glob.iglob('{0}*.jpeg'.format(image_dir)):
+        Image.create(filename=file, answer=re.sub('jpeg','txt', file))
+    
+    for file in glob.iglob('{0}*.jpeg'.format(sec_dir)):
+        Secondary.create(filename=file)
+
