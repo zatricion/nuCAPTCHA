b6db471510be2a8178fdbc34c1212ac24ad38d99
Michael Lauria <mlauria@caltech.edu>
2013-06-24 18:04:44 -0400

    don't care

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
 # flask-peewee database, but could be SQLAlchemy instead.
-from flask_peewee.db import Database
+#from flask_peewee.db import Database
 
 application = Flask(__name__) # had to call it application for elastic beanstalk
-application.config.from_object('config.Configuration')
+#application.config.from_object('config.Configuration')
 
 # instantiate the db wrapper
-db = Database(application)
+#db = Database(application)
 
 # Here I would set up the cache, a task queue, etc.
