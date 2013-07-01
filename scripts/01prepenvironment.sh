a042be5acbdea97c803083cb776d9e60ce981394
Michael Lauria <mlauria@caltech.edu>
2013-07-01 18:29:03 -0400

    new info on root dir

--- scripts/01prepenvironment.sh
+++ scripts/01prepenvironment.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR;
-python ../wtforms-json-master/setup.py install
+python /var/app/current/wtforms-json-master/setup.py install
