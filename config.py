8c5d0b585773959ab017b0a843481f295f1d4954
Michael Lauria <mlauria@caltech.edu>
2013-07-08 17:06:14 -0400

    fixed config, removed psql req

--- config.py
+++ config.py
@@ -3,17 +3,19 @@ import os
 class Configuration(object):  
     
     if 'RDS_HOSTNAME' in os.environ:
-        'engine' : 'peewee.MySQLDatabase',
-        'name' : os.environ['RDS_DB_NAME'],
-        'user' : os.environ['RDS_USERNAME'],
-        'passwd' : os.environ['RDS_PASSWORD'],
-        'host' : os.environ['RDS_HOSTNAME']
+        DATABASE = {
+                'engine': 'peewee.MySQLDatabase',
+                'name': os.environ['RDS_DB_NAME'],
+                'user': os.environ['RDS_USERNAME'],
+                'passwd': os.environ['RDS_PASSWORD'],
+                'host': os.environ['RDS_HOSTNAME']
+        }
     else:
       DATABASE = {
-        'name' : 'nucaptcha',
-        'engine' : 'peewee.MySQLDatabase',
-        'user' : 'root',
-        'host' : 'localhost'
+        'name': 'nucaptcha',
+        'engine': 'peewee.MySQLDatabase',
+        'user': 'root',
+        'host': 'localhost'
       }
     DEBUG = True
     SECRET_KEY = 'ae09rjyh5ijjogej-tw4jtogidfjoigj'
